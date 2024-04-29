d25a4ca859b8a53639351e8137bd1c96092681b6
ekormann <eva.kormann@t-online.de>
2024-04-29 13:43:12 +0200

    Set up initial targets pipeline

---
+++ R/functions.R
@@ -0,0 +1,79 @@
+# Read in and combine all files
+combine_data <- function(path_to_folder) {
+  
+  paths <- list.files(path_to_folder, pattern = "[.]xlsx$", full.names = TRUE)
+  
+  df <- paths %>%
+    
+    set_names(basename) %>%
+    
+    map(readxl::read_excel) %>%
+    
+    list_rbind(names_to = "source_file") %>%
+    
+    # Keep only columns that are not empty
+    keep(~ any(!is.na(.))) %>%
+    
+    # Add new continuous id
+    cbind(newID = 1:nrow(.), .) %>% 
+    
+    # Extract information about survey wave from source file name
+    mutate(Survey = str_extract(source_file, "(?<=\\d{4}_)(.*)(?=-Results)")) %>%
+    mutate(Year = str_extract(source_file, "\\d{4}"))
+  
+  save_path <- "data/processed/combined_data.csv"
+  write_csv(df, save_path)
+  
+  save_path
+}
+
+
+# Produce codebook for all variables
+create_var_overview <- function(path) {
+  df <- read_csv(path, col_names = FALSE, n_max = 1, col_types = "c")
+  
+  out <- df %>%
+    pivot_longer(everything(), names_to = "var_id", values_to = "var_full") %>% 
+    
+    # Add information about instructions to codebook
+    mutate(instruction = case_when(
+      as.integer(sub("X", "", var_id)) >= 15 & 
+        as.integer(sub("X", "", var_id))  <= 23 ~ "agreement",
+      as.integer(sub("X", "", var_id)) >= 24 & 
+        as.integer(sub("X", "", var_id))  <= 37 ~ "institutional occurrence",
+      as.integer(sub("X", "", var_id)) >= 38 & 
+        as.integer(sub("X", "", var_id))  <= 53 ~ "agreement"
+    )) 
+  
+  write_csv(out, "data/processed/var_overview.csv")
+  
+  out
+}
+
+
+process_combined_data <- function(df){
+  
+  # Add institution type and location info to institutional surveys
+  df <- df %>%
+    mutate(X54 = case_when(
+      X63 == "SumDU" ~ "classical",
+      X63 == "DonNU" ~ "classical",
+      X63 == "LPU" ~ "technical",
+      X63 == "LutskNTU" ~ "technical",
+      TRUE ~ as.character(X54)
+      )) %>%
+    mutate(X55 = case_when(
+      X63 == "SumDU" ~ "Sumy Oblast",
+      X63 == "DonNU" ~ "Vinnytsia Oblast",
+      X63 == "LPU" ~ "Lviv Oblast",
+      X63 == "LutskNTU" ~ "Volyn Oblast",
+      # In 2022 question targeted location before displacement in 2022
+      (X63 == "National" & X64 == "2022") ~ X60,
+      TRUE ~ as.character(X55)
+      )) %>% 
+    
+    # Calculate survey duration (total response time in minutes)
+    mutate(duration = as.numeric(difftime(X5, X4, units = "mins")))
+  
+  df
+}
---
+++ _targets.R
@@ -0,0 +1,32 @@
+library(targets)
+library(tarchetypes)
+
+source("R/functions.R")
+
+options(tidyverse.quiet = TRUE)
+
+tar_option_set(packages = c("tidyverse"))
+
+list(
+  tar_target(
+    combined_data_path,
+    combine_data("data/original")
+  ),
+  tar_target(
+    var_overview,
+    create_var_overview(combined_data_path)
+  ),
+  tar_target(
+    combined_data,
+    read_csv(combined_data_path, col_names = FALSE, skip = 1)
+  ),
+  tar_target(
+    processed_data,
+    process_combined_data(combined_data)
+  ),
+  tar_target(
+    save_processed_data,
+    write_csv(processed_data, "data/processed/preprocessed_data.csv")
+  ),
+  tar_render(sample_characteristics, "analysis_notebooks/sample_characteristics.qmd")
+)
